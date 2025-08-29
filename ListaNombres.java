a85df93304cd2264a83f3b7d76bf9a2a9eef935a
Arturo <24103003@alumnos.univo.edu.mx>
2025-08-28 22:50:14 -0600

    Leer nombres y agregarlos mientras no sea salir

--- ListaNombres.java
+++ ListaNombres.java
@@ -1,9 +1,25 @@
-// Commit 1: Crear lista vacía
+// Commit 2: Leer nombres y agregarlos mientras no sea 'salir'
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListaNombres {
     public static void main(String[] args) {
-        // Creamos una lista vacía de tipo String
         ArrayList<String> nombres = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        String nombre;
+        System.out.println("Escribe nombres (escribe 'salir' para terminar):");
+
+        while (true) {
+            nombre = sc.nextLine();
+
+            if (nombre.equalsIgnoreCase("salir")) {
+                break;
+            }
+
+            nombres.add(nombre);
+        }
+
+        sc.close();
     }
 }
