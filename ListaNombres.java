b84d407b05b1910d788a2fa788309b21b836b257
Arturo <24103003@alumnos.univo.edu.mx>
2025-08-28 22:43:55 -0600

    Se creo la lista vacia

---
+++ ListaNombres.java
@@ -0,0 +1,9 @@
+// Commit 1: Crear lista vacía
+import java.util.ArrayList;
+
+public class ListaNombres {
+    public static void main(String[] args) {
+        // Creamos una lista vacía de tipo String
+        ArrayList<String> nombres = new ArrayList<>();
+    }
+}
